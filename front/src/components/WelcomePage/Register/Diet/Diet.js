ee4995bbf45e1cacbc6467c02e485f2255404b4f
marvinxwilson <marvinxwilson@gmail.com>
2020-01-31 13:33:59 +0100

    added workout plan and diet

--- front/src/components/WelcomePage/Register/Diet/Diet.js
+++ front/src/components/WelcomePage/Register/Diet/Diet.js
@@ -3,38 +3,64 @@ import React from 'react'
 import './Diet.css'
 
 class Diet extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            dietName: '',
+            dietGoals: '',
+            mealsPerDay: '',
+            snacksPerDay: '',
+            breakfast: '',
+            breakfasts: []
+        }
+    }
+
+    saveInputValue = (event) => {
+        this.setState({ [event.target.id]: event.target.value })
+    }
+
+    addBreakfastHandler = () => {
+        this.setState(previousState => ({
+            breakfasts: [...previousState.breakfasts, this.state.breakfast]
+        }));
+    }
+
     render() {
+        const breaks = this.state.breakfasts.map((breakfast, i) => {            
+            return (<p className="breakfast-p" key={breakfast + i} >{breakfast}</p>)
+        })
         return (
             <div className="diet register-tap">
                 <h1>Diet</h1>
                 <p>
-                    <label htmlFor="first-name" className="login-label">First Name</label>
-                    <input type="text" id="first-name" className="login-input" onChange={this.saveInputValue} />
+                    <label htmlFor="dietName" className="login-label">Diet Name</label>
+                    <input type="text" id="dietName" className="login-input" onChange={this.saveInputValue} />
                 </p>
                 <p>
-                    <label htmlFor="last-name" className="login-label">Last Name</label>
-                    <input type="text" id="last-name" className="login-input" onChange={this.saveInputValue} />
+                    <label htmlFor="dietGoals" className="login-label">Diet Goals</label>
+                    <input type="number" id="dietGoals" className="login-input" onChange={this.saveInputValue} />
                 </p>
-                <p>
-                    <label htmlFor="birthday" className="login-label">Birthday</label>
-                    <input type="date" id="birthday" className="login-input" onChange={this.saveInputValue} />
-                </p>
-                <p>
-                    <label htmlFor="level" className="login-label">Level</label>
-                    <input type="text" id="level" className="login-input" onChange={this.saveInputValue} />
-                </p>
-                <p>
-                    <label htmlFor="location" className="login-label">Location</label>
-                    <input type="location" id="location" className="login-input" onChange={this.saveInputValue} />
-                </p>
-                <p>
-                    <label htmlFor="email" className="login-label">Email</label>
-                    <input type="email" id="email" className="login-input" onChange={this.saveInputValue} />
-                </p>
-                <p>
-                    <label htmlFor="password" className="login-label">Password</label>
-                    <input type="password" id="password" className="login-input" onChange={this.saveInputValue} />
-                </p>
+
+                <div className='select-meals-number'>
+                    <p>
+                        <label htmlFor="mealsPerDay" className="login-label">Meals per Day</label>
+                        <input type="number" id="mealsPerDay" className="login-input" onChange={this.saveInputValue} />
+                    </p>
+                    <p>
+                        <label htmlFor="snacksPerDay" className="login-label">Snacks per Day</label>
+                        <input type="number" id="snacksPerDay" className="login-input" onChange={this.saveInputValue} />
+                    </p>
+                </div>
+
+                <div>
+                    <label htmlFor="breakfast" className="login-label">Breakfast</label>
+                    <div className="input-button-diet">
+                        <input type="location" id="breakfast" className="login-input" onChange={this.saveInputValue} />
+                        <button onClick={this.addBreakfastHandler}>Add meal</button>
+                    </div>
+                    {this.state.breakfast !== '' ? <div className="breakfast-div">{breaks}</div> : null}
+                </div>
+              
             </div>
         )
     }
--- front/src/components/WelcomePage/Register/WorkoutPlan/WorkoutPlan.js
+++ front/src/components/WelcomePage/Register/WorkoutPlan/WorkoutPlan.js
@@ -1,26 +1,39 @@
 import React from 'react'
 
 import './WorkoutPlan.css'
-import Input from './Input/Input'
 class WorkoutPlan extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            daysAdded: false,
             programName: '',
             status: '',
-            days: ''
+            day1: '',
+            day2: '',
+            day3: '',
+            day4: '',
+            day5: '',
+            day6: '',
+            day7: '',
+            day1rest: false,
+            day2rest: false,
+            day3rest: false,
+            day4rest: false,
+            day5rest: false,
+            day6rest: false,
+            day7rest: false
         }
     }
 
     saveInputValue = (event) => {
         this.setState({ [event.target.id]: event.target.value })
-        console.log(this.state.days)
     }
 
-    
+    restButtonHandler = (event) => {
+        console.log(event.target.id)
+        this.setState({[event.target.id]: true})
+    }
+
     render() {
-        
         return (
             <div className="workout-plan register-tap">
                 <h1>Workout Plan</h1>
@@ -32,12 +45,46 @@ class WorkoutPlan extends React.Component {
                     <label htmlFor="status" className="login-label">Status</label>
                     <input type="text" id="status" className="login-input" onChange={this.saveInputValue} />
                 </p>
-                <div className="workout-days">
-                    <p>
-                        <label htmlFor="days" className="login-label">Days per week? </label>
-                        <input type="text" id="days" className="login-input" onChange={this.saveInputValue} />
-                    </p>
-                    <button onClick={this.dayAdded} className="add-day-btn">Add days</button>
+                <div className='days'>
+                    <h2>Days per week</h2>
+                    {!this.state.day1rest ?  <p>
+                        <label htmlFor="day1" className="login-label">Day 1</label>
+                        <input type="text" id="day1" className="login-input" onChange={this.props.saveInputValue} />
+                        <button id='day1rest' onClick={this.restButtonHandler}>Rest Day</button>
+                    </p> : <p className="rest-day-p">Day 1 is your rest day!</p>}
+
+                    {!this.state.day2rest ?  <p>
+                        <label htmlFor="day2" className="login-label">Day 2</label>
+                        <input type="text" id="day2" className="login-input" onChange={this.props.saveInputValue} />
+                        <button id='day2rest' onClick={this.restButtonHandler}>Rest Day</button>
+                    </p> : <p className="rest-day-p">Day 2 is your rest day!</p>}
+                    {!this.state.day3rest ?  <p>
+                        <label htmlFor="day3" className="login-label">Day 3</label>
+                        <input type="text" id="day3" className="login-input" onChange={this.props.saveInputValue} />
+                        <button id='day3rest' onClick={this.restButtonHandler}>Rest Day</button>
+                    </p> : <p className="rest-day-p">Day 3 is your rest day!</p>}
+                    {!this.state.day4rest ?  <p>
+                        <label htmlFor="day4" className="login-label">Day 4</label>
+                        <input type="text" id="day4" className="login-input" onChange={this.props.saveInputValue} />
+                        <button id='day4rest' onClick={this.restButtonHandler}>Rest Day</button>
+                    </p> : <p className="rest-day-p">Day 4 is your rest day!</p>}
+                    {!this.state.day5rest ?  <p>
+                        <label htmlFor="day5" className="login-label">Day 5</label>
+                        <input type="text" id="day5" className="login-input" onChange={this.props.saveInputValue} />
+                        <button id='day5rest' onClick={this.restButtonHandler}>Rest Day</button>
+                    </p> : <p className="rest-day-p">Day 5 is your rest day!</p>}
+                    {!this.state.day6rest ?  <p>
+                        <label htmlFor="day6" className="login-label">Day 6</label>
+                        <input type="text" id="day6" className="login-input" onChange={this.props.saveInputValue} />
+                        <button id='day6rest' onClick={this.restButtonHandler}>Rest Day</button>
+                    </p> : <p className="rest-day-p">Day 6 is your rest day!</p>}
+                    {!this.state.day7rest ?  <p>
+                        <label htmlFor="day7" className="login-label">Day 7</label>
+                        <input type="text" id="day7" className="login-input" onChange={this.props.saveInputValue} />
+                        <button id='day7rest' onClick={this.restButtonHandler}>Rest Day</button>
+                    </p> : <p className="rest-day-p">Day 7 is your rest day!</p>}
+                   
+                    
                 </div>
             </div>
         )
