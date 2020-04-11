089d5df815bc0e14a8faedfd7ea533ca8588c254
marvinxwilson <marvinxwilson@gmail.com>
2020-04-11 15:10:05 +0200

    builded

---
+++ front/build/precache-manifest.2fbc22ee72e96780df7ebdce0c8896ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e1e8e97f479d61eb21075dd947af59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ec257e16f9c6a94cc46",
+    "url": "/static/css/main.3cb5732b.chunk.css"
+  },
+  {
+    "revision": "cd1ea472ccae1e3f89a8",
+    "url": "/static/js/2.c62e19ee.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.c62e19ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec257e16f9c6a94cc46",
+    "url": "/static/js/main.ccda4047.chunk.js"
+  },
+  {
+    "revision": "1725c4c8b6d385b9be1e",
+    "url": "/static/js/runtime-main.d913ce2b.js"
+  },
+  {
+    "revision": "243dcd13fb94adc119eb432c4f47e4e3",
+    "url": "/static/media/content-background.243dcd13.png"
+  },
+  {
+    "revision": "65042aee14695affe32ca3f03bea4ef7",
+    "url": "/static/media/profile.65042aee.png"
+  }
+]);
