49989bfd77cc2d7e4459d4669e03bd36eb194e9b
marvinxwilson <marvinxwilson@gmail.com>
2020-04-14 20:16:37 +0200

    Update ImageUpload.js

--- front/src/components/ImageUpload/ImageUpload.js
+++ front/src/components/ImageUpload/ImageUpload.js
@@ -3,86 +3,86 @@ import './ImageUpload.css'
 import Button from '../Button/Button'
 import { openUploadPhoto, uploadPhotoUrl } from '../../redux/actions/userActions'
 import { connect } from 'react-redux'
-// import Title from '../Title/Title'
-// import axios from 'axios'
-// const HEROKU_URL = "https://bodybuildingmedia.herokuapp.com/"
+import Title from '../Title/Title'
+import axios from 'axios'
+const HEROKU_URL = "https://bodybuildingmedia.herokuapp.com/"
 
 class ImageUpload extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         images: [],
-    //         imageUrls: ''
-    //     }
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: [],
+            imageUrls: ''
+        }
+    }
 
-    // selectImages = (event) => {
-    //     let images = []
-    //     for (var i = 0; i < event.target.files.length; i++) {
-    //         images[i] = event.target.files.item(i);
-    //     }
-    //     images = images.filter(image => image.name.match(/\.(jpg|jpeg|png|gif)$/))
-    //     this.setState({ images })
-    // }
+    selectImages = (event) => {
+        let images = []
+        for (var i = 0; i < event.target.files.length; i++) {
+            images[i] = event.target.files.item(i);
+        }
+        images = images.filter(image => image.name.match(/\.(jpg|jpeg|png|gif)$/))
+        this.setState({ images })
+    }
 
-    // uploadImages = () => {
-    //     console.log('upload')
-    //     const uploaders = this.state.images.map(image => {
-    //         const data = new FormData();
-    //         data.append("image", image, image.name);
-    //         return axios.post(HEROKU_URL + 'app/v1/files/upload', data, {
-    //             headers: {
-    //                 'Authorization': `Bearer ${localStorage.getItem('jwt')}`
-    //             }
-    //         })
-    //             .then(response => {
-    //                 console.log(response)
-    //                 localStorage.setItem('isPhotoUploaded', "true")
-    //                 this.props.uploadPhotoUrl(HEROKU_URL + response.data.imageUrl)
-    //                 this.setState({
-    //                     imageUrl: response.data.imageUrl
-    //                 });
-    //                 this.props.openUploadPhoto(false)
-    //                 window.location.reload();
-    //             })
-    //             .catch(err => console.log(err))
-    //     });
-    //     // Once all the files are uploaded 
-    //     axios.all(uploaders, {
-    //         headers: {
-    //             'Authorization': `Bearer ${localStorage.getItem('jwt')}`
-    //         }
-    //     }).then(() => {
-    //         console.log('done');
-    //     }).catch(err => alert(err.message));
-    // }
+    uploadImages = () => {
+        console.log('upload')
+        const uploaders = this.state.images.map(image => {
+            const data = new FormData();
+            data.append("image", image, image.name);
+            return axios.post(HEROKU_URL + 'app/v1/files/upload', data, {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+                }
+            })
+                .then(response => {
+                    console.log(response)
+                    localStorage.setItem('isPhotoUploaded', "true")
+                    this.props.uploadPhotoUrl(HEROKU_URL + response.data.imageUrl)
+                    this.setState({
+                        imageUrl: response.data.imageUrl
+                    });
+                    this.props.openUploadPhoto(false)
+                    window.location.reload();
+                })
+                .catch(err => console.log(err))
+        });
+        // Once all the files are uploaded 
+        axios.all(uploaders, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+            }
+        }).then(() => {
+            console.log('done');
+        }).catch(err => alert(err.message));
+    }
 
-    // changePhoto = () => {
-    //     var id = localStorage.getItem('user-id')
-    //     axios.delete(HEROKU_URL + `app/v1/files/images/delete/${id}`, {
-    //         headers: {
-    //             'Authorization': `Bearer ${localStorage.getItem('jwt')}`
-    //         }
-    //     })
-    //         .then(res => {
-    //             console.log(res)
-    //             this.uploadImages()
-    //         })
-    //         .catch(err => {
-    //             console.log(err)
-    //         })
-    // }
+    changePhoto = () => {
+        var id = localStorage.getItem('user-id')
+        axios.delete(HEROKU_URL + `app/v1/files/images/delete/${id}`, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+            }
+        })
+            .then(res => {
+                console.log(res)
+                this.uploadImages()
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
 
     closeUploadPhotoHandler = () => {
         this.props.openUploadPhoto(false)
     }
 
     render() {
-        // var isPhotoUploaded = localStorage.getItem("isPhotoUploaded") === 'true'
+        var isPhotoUploaded = localStorage.getItem("isPhotoUploaded") === 'true'
         return (
             <main className="iu-main">
                 <div className="iu-div">
-                    {/* <Title title="photo upload" />
+                    <Title title="photo upload" />
                     {isPhotoUploaded ? <p className="iu-info-p"><i className="fas fa-exclamation-triangle"></i>
                     Your current photo will be removed!</p> : null}
                     <input className="photo-input " type="file" onChange={this.selectImages} single="true" />
@@ -100,13 +100,13 @@ class ImageUpload extends Component {
                                 label={"upload"}
                                 className="login-btn"
                             />}
-                    </div> */}
-                    <p className='iu-p'>Upload photo under construction!</p>
+                    </div>
+                    {/* <p className='iu-p'>Upload photo under construction!</p>
                     <i className="fas fa-wrench"></i>
                     <Button click={this.closeUploadPhotoHandler}
                             label="Ok"
                             className="close-btn"
-                        />
+                        /> */}
                 </div>
             </main>
         )
